f71a39cc686011deacbd2874b4a6541af4442804
Gabriel Cruz <gabs.oficial98@gmail.com>
2020-06-07 13:27:35 +0200

    Add pydantic models (schemas)

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,37 @@
+from typing import List
 from pydantic import BaseModel
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     username: str
-    # TODO: list of bins
-    # password: str = None
 
 
-class Bin(BaseModel):
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    bins: List[Bin] = []
+
+    class Config():
+        orm_mode = True
+
+
+class BinBase(BaseModel):
     name: str
-    title: str
-    # TODO: password
+    title: str = None
+
+
+class BinCreate(BinBase):
+    owner_id: int
+    password: str
+
+
+class Bin(BinBase):
+    id: int
+    content: str
     # TODO: token?
 
+    class Config():
+        orm_mode = True
